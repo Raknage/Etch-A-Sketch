6bec89da9d50d950a3f9debb5aee3a6d9eb72d1b
Raknage <toni0haila@gmail.com>
2023-01-12 23:18:58 +0200

    Flex squares and initGrid fuction

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const container = document.querySelector(".container");
 const randomColor = () => ((Math.random() * 0xffffff) << 0).toString(16);
+let squareSize = 16;
 
-for (let i = 0; i < 16; i++) {
-  const grid = document.createElement("div");
-  grid.classList.add("grid");
-  for (let i = 0; i < 16; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    square.addEventListener("mouseenter", () => {
-      square.style.backgroundColor = "#" + randomColor();
-    });
-    grid.append(square);
+function initGrid(squareSize) {
+  for (let i = 0; i < squareSize; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    for (let i = 0; i < squareSize; i++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor = "#" + randomColor();
+      });
+      grid.append(square);
+    }
+    container.append(grid);
   }
-  container.append(grid);
 }
+initGrid(squareSize)
