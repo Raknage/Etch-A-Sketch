e8d0d81eeed314d3b129208d13ec254bd2f1a2c7
Raknage <toni0haila@gmail.com>
2023-01-12 22:55:21 +0200

    Random color

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+const randomColor = () => ((Math.random() * 0xffffff) << 0).toString(16);
 
 for (let i = 0; i < 16; i++) {
   const grid = document.createElement("div");
@@ -6,8 +7,10 @@ for (let i = 0; i < 16; i++) {
   for (let i = 0; i < 16; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.addEventListener("mouseenter", () => {
+      square.style.backgroundColor = "#" + randomColor();
+    });
     grid.append(square);
   }
   container.append(grid);
 }
-
