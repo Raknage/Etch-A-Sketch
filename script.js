dc3d17424c1cd10586907808b5702cef7042d446
Raknage <toni0haila@gmail.com>
2023-01-12 22:24:21 +0200

    Grid done

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const container = document.querySelector(".container");
-const grid = document.createElement("div");
 
 for (let i = 0; i < 16; i++) {
+  const grid = document.createElement("div");
+  grid.classList.add("grid");
   for (let i = 0; i < 16; i++) {
     const row = document.createElement("div");
+    row.classList.add("square");
     grid.append(row);
   }
+  container.append(grid);
 }
-
-container.append(grid);
