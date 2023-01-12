a282198d4b7b458501b1898f1d8600d5907e6b4b
Raknage <toni0haila@gmail.com>
2023-01-12 22:29:42 +0200

    Renamed row to square for consistency

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ for (let i = 0; i < 16; i++) {
   const grid = document.createElement("div");
   grid.classList.add("grid");
   for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.add("square");
-    grid.append(row);
+    const square = document.createElement("div");
+    square.classList.add("square");
+    grid.append(square);
   }
   container.append(grid);
 }
+
